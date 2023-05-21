aa65f4786b6938db98b1e33b33ced99526969976
Francemir Feitoza de Lima Sobrinho <francemirl2@gmail.com>
2023-05-21 20:27:40 -0300

    Uptade ex083.py

--- Exercícios Python/ex083.py
+++ Exercícios Python/ex083.py
@@ -1,9 +1,15 @@
 expr = str(input('Digite a expressão: '))
-
-n1 = expr.count('(')
-n2 = expr.count(')')
-
-if n1 == n2:
+pilha = list()
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
     print('Sua expressão está correta!')
 else:
     print('Sua expressão está errada!')
